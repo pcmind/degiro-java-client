760b58d8dda8b6ba938d44e77e7fe66fec1d2fe7
Honoré Vasconcelos <honore.vasconcelos@gmail.com>
2020-04-25 23:49:47 +0100

    refactor: remove unused login parameters

--- src/main/java/cat/indiketa/degiro/model/DLogin.java
+++ src/main/java/cat/indiketa/degiro/model/DLogin.java
@@ -1,16 +1,17 @@
 package cat.indiketa.degiro.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author indiketa
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DLogin {
-
     private String username;
     private String password;
-    private boolean isRedirectToMobile;
-    private String loginButtonUniversal;
 }
