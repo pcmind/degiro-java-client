714fa22b4ba7698b31d359f71c877f611f4c534b
Honoré Vasconcelos <honore.vasconcelos@gmail.com>
2020-05-02 10:19:27 +0100

    fix(api): add missing fields to alerts model

--- src/main/java/cat/indiketa/degiro/model/DAlert.java
+++ src/main/java/cat/indiketa/degiro/model/DAlert.java
@@ -2,8 +2,24 @@ package cat.indiketa.degiro.model;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
 @Data
 public class DAlert {
     private long id;
+    private String title;
     private String text;
+    private LocalDate insertionDate;
+    private OffsetDateTime modificationDate;
+
+    /**
+     * 1: 'urgent',
+     * 2: 'high',
+     * 3: 'medium',
+     * 4: 'low',
+     * 5: 'info'
+     */
+    private int type;
+
 }
